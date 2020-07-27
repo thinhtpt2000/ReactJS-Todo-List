63eae19d365c85014d21f0155da426031c6cb895
thinhtpt2000 <tthinh2128@gmail.com>
2020-07-27 19:20:32 +0700

    use dynamic classname

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import TodoItem from "./components/TodoItem";
 class App extends Component {
   constructor() {
     super();
-    this.todoItems = ["Học JS", "Học Node.js", "Học React"];
+    this.todoItems = [
+      {title: 'Học JS', isComplete: true},
+      {title: 'Học Node.js'},
+      {title: 'Học React'}
+    ];
   }
   render() {
     return (
       <div className="App">
         {this.todoItems.map((item, index) => (
-          <TodoItem key={index} title={item} />
+          <TodoItem key={index} item={item} />
         ))}
       </div>
     );
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import classnames from 'classnames';
+import './TodoItem.css';
 
 class TodoItem extends Component {
     render() {
+        const { item } = this.props;
+        /*
+        let className = 'TodoItem';
+        if (item.isComplete) {
+            className += ' TodoItem-complete';
+        }
+        */
+       let className = classnames('TodoItem', {'TodoItem-complete': item.isComplete});
         return (
-            <div className="TodoItem">
-                <p>{ this.props.title }</p>
+            <div className={className}>
+                <p>{ item.title }</p>
             </div>
         );
     }
