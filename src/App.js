f775bc4e41aeef0941066dae0c72fc854834983d
thinhtpt2000 <tthinh2128@gmail.com>
2020-07-27 16:53:50 +0700

    render a list

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 import TodoItem from "./components/TodoItem";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TodoItem title="Học JS" />
-        <TodoItem title="Học Node.js" />
-        <TodoItem title="Học React" />
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.todoItems = ["Học JS", "Học Node.js", "Học React"];
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.todoItems.map((item, index) => (
+          <TodoItem key={index} title={item} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
