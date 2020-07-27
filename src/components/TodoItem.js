3085f479cf005f922bc7a31d8c60ed96a50bbb0a
thinhtpt2000 <tthinh2128@gmail.com>
2020-07-27 10:05:55 +0700

    add prop to TodoItem component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,9 +4,7 @@ class TodoItem extends Component {
     render() {
         return (
             <div className="TodoItem">
-                <p>Học JS</p>
-                <p>Học Node.js</p>
-                <p>Học React</p>
+                <p>{ this.props.title }</p>
             </div>
         );
     }
